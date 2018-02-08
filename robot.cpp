1eacdbf650b8eb104e1fbf21755035d311b3d65a
Ryan Iman Adami <ImanAdami@users.noreply.github.com>
2018-02-07 16:56:30 -0800

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -13,14 +13,31 @@
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
-
+#include <math.h>
 class Robot : public frc::IterativeRobot {
 public:
 
 	TalonSRX *frontLeftSpeedController, *backLeftSpeedController, *frontRightSpeedController, *backRightSpeedController;
 	Joystick *myStick;
 	SmartDashboard *myData;
-
+	double unitsToSwitch = 30421.93686;
+	double unitAutoTracker = calculateUnitsAuto(140);
+	
+	
+	double calculateUnitsAuto(int distance)
+	{
+		double units = (distance/(6*M_PI))*4096;
+		return units;
+	}
+	
+	void moveForward()
+	{
+		frontLeftSpeedController->Set(ControlMode::PercentOutput, 0.5);
+		backLeftSpeedController->Set(ControlMode::PercentOutput, 0.5);
+		frontRightSpeedController->Set(ControlMode::PercentOutput, 0.5);
+		backRightSpeedController->Set(ControlMode::PercentOutput, 0.5);
+	}
+	
 	void RobotInit() {
 		myStick = new Joystick(0);
 		frontLeftSpeedController = new TalonSRX(2);
@@ -29,39 +46,32 @@ public:
 		backRightSpeedController = new TalonSRX(3);
 		frontRightSpeedController->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Absolute, 0, 0);
 	}
-	/*
-	 * This autonomous (along with the chooser code above) shows how to
-	 * select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to
-	 * the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as
-	 * well.
-	 */
+	
 	void AutonomousInit() override {
-
+		
+		
 	}
-
 	void AutonomousPeriodic() {
-
+		while (unitAutoTracker < unitsToSwitch) 
+		{
+			moveForward();
+		}		
+		//(robot has stopped) shoot cube
+		
 	}
 
 	void TeleopInit() {}
 
 	void TeleopPeriodic() {
-		myData->PutNumber("Pulse Width", frontRightSpeedController->GetSensorCollection().GetPulseWidthVelocity());
+		double PulseWidth = frontRightSpeedController->GetSensorCollection().GetPulseWidthVelocity();
+		myData->PutNumber("Pulse Width", PulseWidth);
 		double leftWheels = myStick->GetRawAxis(5);
 		double rightWheels = myStick->GetRawAxis(1);
 		frontLeftSpeedController->Set(ControlMode::PercentOutput, leftWheels);
 		backLeftSpeedController->Set(ControlMode::PercentOutput, leftWheels);
 		frontRightSpeedController->Set(ControlMode::PercentOutput, rightWheels);
 		backRightSpeedController->Set(ControlMode::PercentOutput, rightWheels);
-
+		
 	}
 
 	void TestPeriodic() {}
