c2db5bb4f95ac5f176de5b40214ce16b8512cd75
Ryan Iman Adami <ImanAdami@users.noreply.github.com>
2018-02-07 17:15:36 -0800

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -14,16 +14,42 @@
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
 #include <math.h>
+
 class Robot : public frc::IterativeRobot {
 public:
 
 	TalonSRX *frontLeftSpeedController, *backLeftSpeedController, *frontRightSpeedController, *backRightSpeedController;
 	Joystick *myStick;
 	SmartDashboard *myData;
+	DoubleSolenoid *gearBoxShifter;
 	double unitsToSwitch = calculateUnitsAuto(140);
 	double PulseWidth = 0;
+	bool solenoidForward;
+	bool solenoidBackward;
 
+	void RobotInit() {
+		myStick = new Joystick(0);
+		frontLeftSpeedController = new TalonSRX(2);
+		frontRightSpeedController = new TalonSRX(1);
+		backLeftSpeedController = new TalonSRX(0);
+		backRightSpeedController = new TalonSRX(3);
+		frontRightSpeedController->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Absolute, 0, 0);
+		gearBoxShifter = new DoubleSolenoid(1,2);
+	}
 
+	void torqueMode() {
+		solenoidForward = myStick->GetRawButton(5);  // LB
+		if (solenoidForward) {
+			gearBoxShifter->Set(DoubleSolenoid::Value::kForward);
+		}
+	}
+
+	void speedMode() {
+		solenoidBackward = myStick->GetRawButton(6); //RB
+		if (solenoidBackward) {
+			gearBoxShifter->Set(DoubleSolenoid::Value::kReverse);
+		}
+	}
 
 	double calculateUnitsAuto(int distance)
 	{
@@ -37,15 +63,6 @@ public:
 		backLeftSpeedController->Set(ControlMode::PercentOutput, 0.5);
 		frontRightSpeedController->Set(ControlMode::PercentOutput, 0.5);
 		backRightSpeedController->Set(ControlMode::PercentOutput, 0.5);
-	}
-
-	void RobotInit() {
-		myStick = new Joystick(0);
-		frontLeftSpeedController = new TalonSRX(2);
-		frontRightSpeedController = new TalonSRX(1);
-		backLeftSpeedController = new TalonSRX(0);
-		backRightSpeedController = new TalonSRX(3);
-		frontRightSpeedController->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Absolute, 0, 0);
 	}
 
 	void AutonomousInit() override {
@@ -63,7 +80,9 @@ public:
 	void TeleopInit() {}
 
 	void TeleopPeriodic() {
-
+		if (speedMode) {
+			shiftGearBox();
+		}
 		double leftWheels = myStick->GetRawAxis(5);
 		double rightWheels = myStick->GetRawAxis(1);
 		frontLeftSpeedController->Set(ControlMode::PercentOutput, leftWheels);
